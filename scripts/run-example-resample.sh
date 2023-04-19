5375d7572493bf5f400aef4e16f6a55a0c57d58e
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-04-19 19:05:30 +0200

    R!! remove resampling, probably no time for it anyways

--- scripts/run-example-resample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-project_dir=$(realpath "$(dirname $(dirname "${BASH_SOURCE[0]}"))")
-pushd "$project_dir" || exit
-
-mkdir -p out
-resample-stl1 resources/stl1_era5land_long_20211111_20211121.nc EU500M_E048N012T6 out
-
-popd || exit
